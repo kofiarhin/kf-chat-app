5aeb89f9d67d2b938c7251ea5f0b6a8ecfaa86a2
kofi <kofiarhin69@gmail.com>
2024-03-01 04:09:43 +0000

    updated: upadted the entire code base both frontend and backend

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const server = http.createServer(app);
 // setup middiewrae
 
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
@@ -21,23 +22,51 @@ const io = new Server(server, {
 });
 
 let messages = [];
+let users = [];
 
 io.on("connection", (socket) => {
   io.emit("receive_message", messages);
 
-  socket.on("join", (data) => {
-    socket.broadcast.emit("new_user", data);
+  socket.on("check_user", (username) => {
+    const checkUser = users.find((user) => user === username);
+    socket.emit("check_user", {
+      error: checkUser ? true : false,
+      message: checkUser ? "user already exist" : "",
+    });
+  });
+
+  socket.on("join", (username) => {
+    console.log("new user just joined");
+    users.push(username);
+    console.log(users);
+    socket.broadcast.emit("new_user", username);
+    io.emit("users", users);
   });
 
   socket.on("leave", (data) => {
+    console.log(data);
+    // remove user from list of users
+    users = users.filter((user) => user !== data);
+    console.log(users);
+    socket.broadcast.emit("users", users);
     socket.broadcast.emit("user_left", data);
   });
+
   socket.on("send_message", (data) => {
     messages.push(data);
     io.emit("receive_message", messages);
   });
 });
 
+// routes
+app.post("/checkUser", (req, res) => {
+  const { username } = req.body;
+  const checkUser = users.includes(username);
+
+  return res.json({ message: checkUser });
+});
+
+// production code
 if (process.env.NODE_ENV == "production") {
   const publicPath = path.resolve(__dirname, ".", "build");
   const filePath = path.join(__dirname, ".", "build", "index.html");
