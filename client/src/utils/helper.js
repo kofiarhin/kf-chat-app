5d2dcb770acab0f00cd80fe8abce7ac8e18a5e9a
kofi <kofiarhin69@gmail.com>
2024-03-01 05:45:53 +0000

    update

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,15 +1,15 @@
 import io from "socket.io-client";
 
 // production socket
-// const socket = io.connect("https://kf-chat-app.onrender.com/");
+const socket = io.connect("https://kf-chat-app.onrender.com/");
 
 // dev socket
 // const socket = io.connect("http://localhost:5000");
 
-const socket = io.connect(
-  process.env.REACT_APP_NODE_ENV === "production"
-    ? process.env.REACT_APP_SOCKET_IO_URL_PROD
-    : process.env.REACT_APP_SOCKET_IO_URL_DEV
-);
+// const socket = io.connect(
+//   process.env.REACT_APP_NODE_ENV === "production"
+//     ? process.env.REACT_APP_SOCKET_IO_URL_PROD
+//     : process.env.REACT_APP_SOCKET_IO_URL_DEV
+// );
 
 export { socket };
