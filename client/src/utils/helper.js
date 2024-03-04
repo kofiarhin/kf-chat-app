2576cd8d3af523a76222e2cbb7d546f2c62c39ed
kofi <kofiarhin69@gmail.com>
2024-03-04 11:09:25 +0000

    update code clean up

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -5,7 +5,7 @@ const uploadImage = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
   formData.append("upload_preset", "gzbuxpwt");
-  const url = "http://api.cloudinary.com/v1_1/dlsiabgiw/image/upload";
+  const url = "https://api.cloudinary.com/v1_1/dlsiabgiw/image/upload";
 
   const response = await fetch(url, {
     method: "POST",
