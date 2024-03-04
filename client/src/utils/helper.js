346aa73691f35e96290075e6fcacf3911666159a
kofi <kofiarhin69@gmail.com>
2024-03-04 01:51:03 +0000

    update

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,9 +1,9 @@
 import io from "socket.io-client";
 
 // production socket
-// const socket = io.connect("https://kf-chat-app.onrender.com/");
+const socket = io.connect("https://kf-chat-app.onrender.com/");
 
 // devevlopment socket
-const socket = io.connect("http://localhost:5000");
+// const socket = io.connect("http://localhost:5000");
 
 export { socket };
